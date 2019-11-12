6334b572bf29201c6495aca8c9d4d9c000180c0e
takatoku23 <takutoplayer1211@icloud.com>
2019-11-12 11:43:20 +0900

    fix indention

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
-before_save { email.downcase! }
+  before_save { email.downcase! }
 
-validates :name, presence: true, length: { maximum: 50 }
-  # REGEX = Regular Expression
-  #To validate correct email structure
-EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
+    # REGEX = Regular Expression
+    #To validate correct email structure
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-validates :email, presence: true, length: { maximum: 50 },
-format: { with: EMAIL_REGEX },
-uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 50 },
+  format: { with: EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
 
-has_secure_password
-validates :password, length: {minimum: 6},allow_nil: true
+  has_secure_password
+  validates :password, length: {minimum: 6},allow_nil: true
 end
