8d4495a8598d576ed1a57131bdc1073756625781
takatoku23 <takutoplayer1211@icloud.com>
2019-11-12 11:36:41 +0900

    fix indntion and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,16 @@ def new
   @user = User.new
 end
 def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_url
-    else
-      render "new"
-    end
+  @user = User.new(user_params)
+  if @user.save
+    flash[:success] = "Create new account"
+    redirect_to root_url
+  else
+    render "new"
+  end
 end
 private
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    before_save { email.downcase! }
+before_save { email.downcase! }
 
 validates :name, presence: true, length: { maximum: 50 }
   # REGEX = Regular Expression
