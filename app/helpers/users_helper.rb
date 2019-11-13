743bdce7a39fcdde51b3d63d6c0232543dec9f5d
takatoku23 <57470668+takatoku23@users.noreply.github.com>
2019-11-13 20:48:38 +0800

    create-user-setting (#7)
    * create-user-setting
    
    * create-user-setting
    
    * fix all form head align center

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
     #Generate hex taken
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url,alt:user.name, class:"gravatar")
+    image_tag(gravatar_url,alt:user.name,class:"photo")
   end
 end
