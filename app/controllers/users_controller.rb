1bb81b75e668df3f3d809828f6b0a09919d695cd
takatoku23 <takutoplayer1211@icloud.com>
2019-11-13 13:41:33 +0900

    create-user-setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,26 @@ end
 def show
   @user = User.find(params[:id])
 end
+def edit
+  @user = User.find(params[:id])
+end
+def update
+  @user = User.find(params[:id])
+  if current_user == @user
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to user_path(current_user)
+    else
+      flash.now[:danger] = 'Profile notupdated'
+      render 'edit'
+    end   
+  
+  else
+      redirect_to root_url
+  end
+end
 private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 end
