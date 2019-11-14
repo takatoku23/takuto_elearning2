368b175f6b6b82cf3187ee525aa21430bef825d4
takatoku23 <takutoplayer1211@icloud.com>
2019-11-14 11:16:26 +0900

    fix users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user,class_name)
     #Generate hex taken
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url,alt:user.name,class:"photo")
+    image_tag(gravatar_url,alt:user.name,class:class_name)
   end
 end
