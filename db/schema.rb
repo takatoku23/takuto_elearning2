13d3e1494f8f790464da400ef44620cef7b0994b
takatoku23 <takutoplayer1211@icloud.com>
2019-11-19 11:44:32 +0900

    create-category-index-page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_080552) do
+ActiveRecord::Schema.define(version: 2019_11_19_012034) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
