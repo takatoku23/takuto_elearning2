730e2aa3a9393b2560e7baa92468e55dcdf4a75d
takatoku23 <57470668+takatoku23@users.noreply.github.com>
2019-11-19 12:13:47 +0800

    create-new-category-page (#13)
    * create-new-category-page
    
    * fix indention

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,23 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page],per_page: 10)
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Create new category"
+      redirect_to admin_categories_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 end
