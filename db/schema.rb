d4671df041be59a102ec73e947d34a3c4d749328
takatoku23 <takutoplayer1211@icloud.com>
2019-11-19 12:09:34 +0900

    add button change coloum type

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_012034) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
