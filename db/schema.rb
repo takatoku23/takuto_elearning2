e172fa69c130d3bae99a9a34da3d3dbf24d53dec
takatoku23 <takutoplayer1211@icloud.com>
2019-11-20 13:19:09 +0900

    create-new-word-form

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_010602) do
+ActiveRecord::Schema.define(version: 2019_11_20_021750) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "choices", force: :cascade do |t|
+    t.integer "word_id"
+    t.string "content"
+    t.boolean "isCorrect", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
