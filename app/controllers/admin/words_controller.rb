0af9904dd847e4f0f77aee83639e2eee16c926dc
takatoku23 <takutoplayer1211@icloud.com>
2019-11-20 13:19:09 +0900

    create-new-word-form

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -4,7 +4,23 @@ class Admin::WordsController < ApplicationController
   end
 
   def new
-    @word = Word.new
+    @category = Category.find(params[:category_id])
+    @word = @category.words.build
+    3.times{@word.choices.build}
   end
 
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.build(word_params)
+    if @word.save
+      flash[:success] = "Create new word"
+      redirect_to admin_category_words_path
+    else
+      render "new"
+    end
+  end
+  private
+  def word_params
+    params.require(:word).permit(:category_id, :content, choices_attributes:[:word_id, :content, :isCorrect])
+  end
 end
