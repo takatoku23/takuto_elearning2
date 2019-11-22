80303032fa4f0a4dfb2b65fc9558762119e55713
takatoku23 <takutoplayer1211@icloud.com>
2019-11-22 11:46:32 +0900

    create-lesson-layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_021750) do
+ActiveRecord::Schema.define(version: 2019_11_21_102227) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2019_11_20_021750) do
     t.integer "word_id"
     t.string "content"
     t.boolean "isCorrect", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
