3294b1550588f1ae6ae13458b61e3e1b8d07de82
takatoku23 <takutoplayer1211@icloud.com>
2019-11-29 10:04:39 +0900

    lesson-filtering

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,11 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page],per_page: 6)
+    @status = params[:status]
+    if @status == "1"
+      @categories = current_user.categories.paginate(page: params[:page],per_page: 6)
+    elsif @status == "0"
+      @categories = @categories.where.not(id: current_user.category_ids).paginate(page: params[:page],per_page: 6)
+    end
   end
 end
